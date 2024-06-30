ae5f0d6c64c076f908dfdd40c3b80401880fa16d
nqtruowng <nguyenquangtruong26012003@gmail.com>
2024-07-01 01:12:48 +0700

    update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,15 @@
-import Home from '../pages/Home/Home'
-import News from '../pages/News/News'
-import Contact from '../pages/Contact/Contact'
-import Present from '../pages/Present/Present'
-import Products from '../pages/Products/Products'
+import Home from '../pages/Home'
+import News from '../pages/News'
+import Contact from '../pages/Contact'
+import Present from '../pages/Present'
+import Products from '../pages/Products'
 
 const publicRoutes = [
-    { path: '/', component: Home },
-    { path: '/products', component: Products },
-    { path: '/news', component: News },
-    { path: '/contact', component: Contact },
-    { path: '/present', component: Present },
+    { path: '/', component: <Home /> },
+    { path: '/products', component: <Products /> },
+    { path: '/news', component: <News /> },
+    { path: '/contact', component: <Contact /> },
+    { path: '/present', component: <Present /> },
 ]
 
 export { publicRoutes }
